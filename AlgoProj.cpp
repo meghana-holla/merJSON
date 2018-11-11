53b56339845f901c77da07fbc609d850d48a04bb
meghana-holla <meghana.holla@gmail.com>
2018-11-11 20:18:20 +0530

    added json tree construction and representation

--- AlgoProj.cpp
+++ AlgoProj.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 #include<fstream>
+#include<string.h>
 using namespace std;
+#define MAX_NUMBER_OF_CHILDREN 10
+
+typedef struct node
+{
+  int depth;
+  int flag; // tells us if the inner value is a list of nodes or string
+  char* name;
+  char* value;
+  struct node* innerNodes[MAX_NUMBER_OF_CHILDREN]; // list of nodes under it
+  int no_of_children;
+} Node;
 
 char* readfile(int *i)
 {
@@ -24,8 +36,40 @@ char* readfile(int *i)
   return string;
 }
 
-void genjson(char* json, int len, int* index, int depth)
+Node* initNode(int depth)
 {
+  Node* r = (Node*)malloc(sizeof(Node));
+  for(int i=0;i<MAX_NUMBER_OF_CHILDREN;i++)
+  {
+    r->innerNodes[i]=NULL;
+  }
+  r->no_of_children=0;
+  r->depth = depth;
+  return r;
+}
+
+Node* addNode(Node* parent, int flag, void* para)
+{
+  if(flag==1)
+  {
+    Node* child = (Node*)para;
+    parent->innerNodes[parent->no_of_children] = child;
+    parent->no_of_children += 1;
+    parent->flag=1;
+  }
+  else
+  {
+    char* value = (char*)para;
+    parent->value = (char*)malloc((strlen(value)+1)*sizeof(char));
+    strcpy(parent->value,value);
+    parent->flag=0;
+  }
+}
+
+void genjson(char* json, int len, int* index, int depth, Node* parent)
+{
+  Node* child = initNode(depth);
+
   char name[100];
   char value[100];
   name[0]='\0';
@@ -67,6 +111,9 @@ void genjson(char* json, int len, int* index, int depth)
     *index+=1;
   }
   name[t] = '\0';
+  child->name = (char*)malloc(sizeof(name));
+  strcpy(child->name,name);
+  addNode(parent,1,(void*)child);
   *index+=1;
 
   while(*index<len)
@@ -81,7 +128,7 @@ void genjson(char* json, int len, int* index, int depth)
     if(json[*index]=='{')
     {
       *index+=1;
-      genjson(json, len, index, depth+1);
+      genjson(json, len, index, depth+1, child);
       *index-=1;
       inrec=1;
       break;
@@ -110,6 +157,7 @@ void genjson(char* json, int len, int* index, int depth)
     {
       *index += 1;
       cout << name << "-" << value << "-" << depth << "\n";
+      addNode(child,0,(void*)value);
       return;
     }
     if(json[*index]==',') break;
@@ -118,9 +166,37 @@ void genjson(char* json, int len, int* index, int depth)
   *index+=1;
 
   cout << name << "-" << value << "-" << depth << "\n";
+  addNode(child,0,(void*)value);
   if(*index<len)
   {
-    return genjson(json, len, index, depth);
+    return genjson(json, len, index, depth, parent);
+  }
+}
+
+/*
+
+typedef struct node
+{
+  int depth;
+  int flag; // tells us if the inner value is a list of nodes or string
+  char* name;
+  char* value;
+  struct node* innerNodes[MAX_NUMBER_OF_CHILDREN]; // list of nodes under it
+  int no_of_children;
+} Node;
+
+*/
+
+void print_tree(node* root)
+{
+  if(root==NULL)
+  {
+    return;
+  }
+  if(root->depth>-1) cout << string(root->depth*2,'\t') << root->name << ":" << root->value <<endl;
+  for(int i=0;i<root->no_of_children;i++)
+  {
+    print_tree(root->innerNodes[i]);
   }
 }
 
@@ -133,5 +209,10 @@ int main()
   char name[100];
   char value[100];
   int index=0;
-  genjson(fullfile, length, &index, 0);
+  Node* root = initNode(-1);
+  root->name = (char*)malloc(100);
+  strcpy(root->name,"~");
+  genjson(fullfile, length, &index, 0, root);
+  cout << endl << endl << endl << endl;
+  print_tree(root);
 }

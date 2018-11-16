8c04f6384e0a76e4511de29f990bd06a5a0b34c2
Nishant Aklecha <31594715+Naklecha@users.noreply.github.com>
2018-11-16 14:01:09 +0530

    Add files via upload

--- AlgoProj.cpp
+++ AlgoProj.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<string.h>
+#include<stdlib.h>
 using namespace std;
 #define MAX_NUMBER_OF_CHILDREN 10
 
@@ -14,10 +15,37 @@ typedef struct node
   int no_of_children;
 } Node;
 
-char* readfile(int *i)
+void mergit(Node* base, Node* head)
+{
+  if(head==NULL)
+  {
+    return;
+  }
+  for(int i=0; i<base->no_of_children; i++)
+  {
+    if(strcmp(base->innerNodes[i]->name,head->name)==0)
+    {
+        for(int j=0; j<head->no_of_children; j++)
+        {
+          mergit(base->innerNodes[i],head->innerNodes[j]);
+        }
+        return;
+    }
+  }
+  base->innerNodes[base->no_of_children++]=head;
+}
+
+void merge(Node* base, Node* head)
+{
+  for(int j=0; j<head->no_of_children; j++)
+  {
+    mergit(base,head->innerNodes[j]);
+  }
+}
+
+char* readfile(int *i, const char fileName[100])
 {
   char ch;
-	const char *fileName="people.json";
 	ifstream file;
 	file.open(fileName,ios::in);
   char* string = (char*)malloc(100000);
@@ -173,46 +201,51 @@ void genjson(char* json, int len, int* index, int depth, Node* parent)
   }
 }
 
-/*
-
-typedef struct node
-{
-  int depth;
-  int flag; // tells us if the inner value is a list of nodes or string
-  char* name;
-  char* value;
-  struct node* innerNodes[MAX_NUMBER_OF_CHILDREN]; // list of nodes under it
-  int no_of_children;
-} Node;
-
-*/
-
 void print_tree(node* root)
 {
+  int count=0;
   if(root==NULL)
   {
     return;
   }
-  if(root->depth>-1) cout << string(root->depth*2,'\t') << root->name << ":" << root->value <<endl;
+  cout << "\"" <<root->name <<"\"" << " : ";//<<endl;
+  if(root->no_of_children>0) cout<<"{";
+  else cout<< "\"" << root->value<<"\"";
   for(int i=0;i<root->no_of_children;i++)
   {
     print_tree(root->innerNodes[i]);
+    count++;
+    if(count<root->no_of_children) cout<< ",";
+    else cout<<"}";
   }
 }
 
 int main()
 {
-  int length;
-  char* fullfile = readfile(&length);
-  cout << fullfile << "\n"<< length << "\n";
+  int length1;
+  int length2;
+  char* fullfile1 = readfile(&length1,"base.json");
+  cout << fullfile1 << "\n"<< length1 << "\n";
+  char* fullfile2 = readfile(&length2,"head.json");
+  cout << fullfile2 << "\n"<< length2 << "\n";
 
   char name[100];
   char value[100];
-  int index=0;
-  Node* root = initNode(-1);
-  root->name = (char*)malloc(100);
-  strcpy(root->name,"~");
-  genjson(fullfile, length, &index, 0, root);
+  int index1=0;
+  int index2=0;
+
+  Node* base = initNode(-1);
+  base->name = (char*)malloc(10);
+  strcpy(base->name,"~");
+
+  Node* head = initNode(-1);
+  head->name = (char*)malloc(10);
+  strcpy(head->name,"~");
+
+  genjson(fullfile1, length1, &index1, 0, base);
+  genjson(fullfile2, length2, &index2, 0, head);
+
+  merge(base,head);
   cout << endl << endl << endl << endl;
-  print_tree(root);
+  print_tree(base);
 }
